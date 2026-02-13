cc40e260a3618a6d29226eac5f25fa63432f7243
dasbaum-ch <info@dasbaum.ch>
2026-02-13 16:47:59 +0100

    rename exporter.py

--- exporter/__init__.py
+++ exporter/__init__.py
@@ -1,6 +1,6 @@
 """Redfish Prometheus Exporter: Collect and export power metrics from Redfish-compliant hosts."""
 
-from exporter.exporter import run_exporter
+from exporter.main import run_exporter
 from exporter.config import HostConfig, RedfishSessionState, PowerMetrics
 from exporter.redfish import RedfishHost
 
